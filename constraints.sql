b71ca59adf06dd0f9bf0ec259cab75e711811dcc
jeroen <jerrik1998@gmail.com>
2019-12-02 09:23:49 +0100

    capetals

--- constraints.sql
+++ constraints.sql
@@ -1,51 +1,51 @@
-DROP trigger if exists TR_participant_in_knowledge_session;
+DROP TRIGGER IF EXISTS TR_participant_in_knowledge_session;
 
 DELIMITER $$
-CREATE trigger TR_participant_in_knowledge_session
-before insert
-on
+CREATE TRIGGER TR_participant_in_knowledge_session
+BEFORE INSERT
+ON
 participant_of_campaign
-for each row
+FOR EACH ROW
 BEGIN
-  if ((select campaign.CAMPAIGN_TYPE from campaign where campaign.NAME = NEW.NAME) != 'conferentie') THEN
-		if((select count(*) from knowledge_session where knowledge_session.PARTICIPANTID = NEW.PARTICIPANTID) = 0) Then
-            signal sqlstate '45000' set message_text = 'no nickname available for user of knowledge session';
-        end if;
-  end if;
+  IF ((SELECT campaign.CAMPAIGN_TYPE FROM campaign WHERE campaign.NAME = NEW.NAME) != 'conferentie') THEN
+		IF ((SELECT count(*) FROM knowledge_session WHERE knowledge_session.PARTICIPANTID = NEW.PARTICIPANTID) = 0) Then
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'no nickname available for user of knowledge session';
+        END IF;
+  END IF;
 END$$
 
-DROP procedure if exists SP_multiplechoice_answers;
+DROP PROCEDURE IF EXISTS SP_multiplechoice_answers;
 
 DELIMITER $$
 CREATE PROCEDURE SP_multiplechoice_answers (IN question_id INT)
 BEGIN
     IF((SELECT ((SELECT COUNT(*)
         FROM tmp_multiple_choice_question
-        where QUESTIONID = question_id)
+        WHERE QUESTIONID = question_id)
         +
         (SELECT COUNT(*)
         FROM multiple_choice_question
-        where QUESTIONID = question_id))
+        WHERE QUESTIONID = question_id))
         ) < 2) THEN
         DELETE FROM tmp_multiple_choice_question where QUESTIONID = question_id;
-        signal sqlstate '45000' set message_text = 'A multiple choice question must have two or more answers';
-    end if;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'A multiple choice question must have two or more answers';
+    END IF;
 
     IF((SELECT ((SELECT COUNT(*)
         FROM tmp_multiple_choice_question
-        where QUESTIONID = question_id AND IS_CORRECT = 1)
+        WHERE QUESTIONID = question_id AND IS_CORRECT = 1)
             +
         (SELECT COUNT(*)
         FROM multiple_choice_question
-        where QUESTIONID = question_id AND IS_CORRECT = 1)))
+        WHERE QUESTIONID = question_id AND IS_CORRECT = 1)))
         != 1) THEN
         DELETE FROM tmp_multiple_choice_question where QUESTIONID = question_id;
-        signal sqlstate '45000' set message_text = 'There cannot be more or less then one correct answer';
-    end if;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'There cannot be more or less then one correct answer';
+    END IF;
 
     INSERT INTO multiple_choice_question
     SELECT * FROM tmp_multiple_choice_question
-    where QUESTIONID = question_id;
+    WHERE QUESTIONID = question_id;
 
-    DELETE FROM tmp_multiple_choice_question where QUESTIONID = question_id;
+    DELETE FROM tmp_multiple_choice_question WHERE QUESTIONID = question_id;
 END$$
