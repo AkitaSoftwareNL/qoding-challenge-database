fa90cf88437aa3b664d2e0ec3072d10cb107792e
jeroen <jerrik1998@gmail.com>
2019-12-10 14:32:15 +0100

    fixed stuff

--- DLL.sql
+++ DLL.sql
@@ -1,4 +1,4 @@
-/*Review script*/
+/*test for stuff*/
 /*delete from given_answer where PARTICIPANTID = 1*/
 
 DELETE FROM given_answer;
@@ -99,184 +99,184 @@ VALUES (1, 5, 1, 1, 'Hello world'),
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 6, 1, 1, 'U'),
        (2, 6, 1, 1, 'N'),
-       (3, 6, 1, 1, 'Ja'),
+       (3, 6, 1, 2, 'Ja'),
        (4, 6, 1, 1, 'H'),
        (5, 6, 1, 1, 'p'),
-       (6, 6, 1, 1, 'Tweede'),
+       (6, 6, 1, 3, 'Tweede'),
        (7, 6, 1, 1, 'I'),
        (8, 6, 1, 1, 'b'),
        (9, 6, 1, 1, 'A'),
        (10, 6, 1, 1, 's'),
        (11, 6, 1, 1, 'L'),
-       (12, 6, 1, 1, 'waar');
+       (12, 6, 1, 2, 'waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 7, 1, 1, 'T'),
        (2, 7, 1, 1, 'M'),
-       (3, 7, 1, 1, 'Ja'),
+       (3, 7, 1, 2, 'Ja'),
        (4, 7, 1, 1, 'a'),
        (5, 7, 1, 1, 'N'),
-       (6, 7, 1, 1, 'Beide'),
+       (6, 7, 1, 2, 'Beide'),
        (7, 7, 1, 1, 'N'),
        (8, 7, 1, 1, 'i'),
        (9, 7, 1, 1, 'U'),
        (10, 7, 1, 1, 'n'),
        (11, 7, 1, 1, 'r'),
-       (12, 7, 1, 1, 'Waar');
+       (12, 7, 1, 2, 'Waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 8, 1, 1, 'd'),
        (2, 8, 1, 1, 'A'),
-       (3, 8, 1, 1, 'Nee'),
+       (3, 8, 1, 3, 'Nee'),
        (4, 8, 1, 1, 'y'),
        (5, 8, 1, 1, 'l'),
-       (6, 8, 1, 1, 'Eerst'),
+       (6, 8, 1, 3, 'Eerst'),
        (7, 8, 1, 1, 'J'),
        (8, 8, 1, 1, 'T'),
        (9, 8, 1, 1, 't'),
        (10, 8, 1, 1, 'g'),
        (11, 8, 1, 1, 'U'),
-       (12, 8, 1, 1, 'Niet waar');
+       (12, 8, 1, 3, 'Niet waar');
 
 
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 9, 1, 1, 'I'),
        (2, 9, 1, 1, 'T'),
-       (3, 9, 1, 1, 'Nee'),
+       (3, 9, 1, 3, 'Nee'),
        (4, 9, 1, 1, 'G'),
        (5, 9, 1, 1, 'n'),
-       (6, 9, 1, 1, 'Error'),
+       (6, 9, 1, 3, 'Error'),
        (7, 9, 1, 1, 'D'),
        (8, 9, 1, 1, 'C'),
        (9, 9, 1, 1, 'c'),
        (10, 9, 1, 1, 'H'),
        (11, 9, 1, 1, 'p'),
-       (12, 9, 1, 1, 'waar');
+       (12, 9, 1, 2, 'waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 10, 1, 1, 'X'),
        (2, 10, 1, 1, 'P'),
-       (3, 10, 1, 1, 'Ja'),
+       (3, 10, 1, 2, 'Ja'),
        (4, 10, 1, 1, 'X'),
        (5, 10, 1, 1, 'I'),
-       (6, 10, 1, 1, 'Error'),
+       (6, 10, 1, 3, 'Error'),
        (7, 10, 1, 1, 'S'),
        (8, 10, 1, 1, 'x'),
        (9, 10, 1, 1, 'L'),
        (10, 10, 1, 1, 'Y'),
        (11, 10, 1, 1, 'D'),
-       (12, 10, 1, 1, 'waar');
+       (12, 10, 1, 2, 'waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 11, 1, 1, 'H'),
        (2, 11, 1, 1, 'P'),
-       (3, 11, 1, 1, 'Nee'),
+       (3, 11, 1, 3, 'Nee'),
        (4, 11, 1, 1, 'h'),
        (5, 11, 1, 1, 'S'),
-       (6, 11, 1, 1, 'Beide'),
+       (6, 11, 1, 2, 'Beide'),
        (7, 11, 1, 1, 'T'),
        (8, 11, 1, 1, 'P'),
        (9, 11, 1, 1, 'd'),
        (10, 11, 1, 1, 'A'),
        (11, 11, 1, 1, 'A'),
-       (12, 11, 1, 1, 'Waar');
+       (12, 11, 1, 2, 'Waar');
 
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 12, 1, 1, 'N'),
        (2, 12, 1, 1, 'b'),
-       (3, 12, 1, 1, 'Ja'),
+       (3, 12, 1, 2, 'Ja'),
        (4, 12, 1, 1, 'k'),
        (5, 12, 1, 1, 'D'),
-       (6, 12, 1, 1, 'Error'),
+       (6, 12, 1, 3, 'Error'),
        (7, 12, 1, 1, 'r'),
        (8, 12, 1, 1, 'K'),
        (9, 12, 1, 1, 'r'),
        (10, 12, 1, 1, 'M'),
        (11, 12, 1, 1, 'c'),
-       (12, 12, 1, 1, 'Waar');
+       (12, 12, 1, 2, 'Waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 5, 2, 1, 'A'),
        (2, 5, 2, 1, 'k'),
-       (3, 5, 2, 1, 'Nee'),
+       (3, 5, 2, 3, 'Nee'),
        (4, 5, 2, 1, 'r'),
        (5, 5, 2, 1, 'B'),
-       (6, 5, 2, 1, 'Beide'),
+       (6, 5, 2, 2, 'Beide'),
        (7, 5, 2, 1, 'X'),
        (8, 5, 2, 1, 'd'),
        (9, 5, 2, 1, 'h'),
        (10, 5, 2, 1, 'g'),
        (11, 5, 2, 1, 'Q'),
-       (12, 5, 2, 1, 'Niet waar');
+       (12, 5, 2, 3, 'Niet waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 6, 2, 1, 'W'),
        (2, 6, 2, 1, 'd'),
-       (3, 6, 2, 1, 'Ja'),
+       (3, 6, 2, 2, 'Ja'),
        (4, 6, 2, 1, 'm'),
        (5, 6, 2, 1, 'I'),
-       (6, 6, 2, 1, 'Eerste'),
+       (6, 6, 2, 3, 'Eerste'),
        (7, 6, 2, 1, 'X'),
        (8, 6, 2, 1, 't'),
        (9, 6, 2, 1, 'z'),
        (10, 6, 2, 1, 'R'),
        (11, 6, 2, 1, 'e'),
-       (12, 6, 2, 1, 'Waar');
+       (12, 6, 2, 2, 'Waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 7, 2, 1, 'E'),
        (2, 7, 2, 1, 'C'),
-       (3, 7, 2, 1, 'Nee'),
+       (3, 7, 2, 3, 'Nee'),
        (4, 7, 2, 1, 'u'),
        (5, 7, 2, 1, 'M'),
-       (6, 7, 2, 1, 'Tweede'),
+       (6, 7, 2, 3, 'Tweede'),
        (7, 7, 2, 1, 'n'),
        (8, 7, 2, 1, 'N'),
        (9, 7, 2, 1, 'x'),
        (10, 7, 2, 1, 'e'),
        (11, 7, 2, 1, 'I'),
-       (12, 7, 2, 1, 'Waar');
+       (12, 7, 2, 2, 'Waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 8, 2, 1, 'P'),
        (2, 8, 2, 1, 'Y'),
-       (3, 8, 2, 1, 'Ja'),
+       (3, 8, 2, 2, 'Ja'),
        (4, 8, 2, 1, 'A'),
        (5, 8, 2, 1, 'J'),
-       (6, 8, 2, 1, 'Beide'),
+       (6, 8, 2, 2, 'Beide'),
        (7, 8, 2, 1, 'R'),
        (8, 8, 2, 1, 'p'),
        (9, 8, 2, 1, 'a'),
        (10, 8, 2, 1, 'v'),
        (11, 8, 2, 1, 'w'),
-       (12, 8, 2, 1, 'Waar');
+       (12, 8, 2, 2, 'Waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 5, 3, 1, 'm'),
        (2, 5, 3, 1, 'G'),
-       (3, 5, 3, 1, 'Ja'),
+       (3, 5, 3, 2, 'Ja'),
        (4, 5, 3, 1, 'r'),
        (5, 5, 3, 1, 'S'),
-       (6, 5, 3, 1, 'Error'),
+       (6, 5, 3, 3, 'Error'),
        (7, 5, 3, 1, 'c'),
        (8, 5, 3, 1, 'a'),
        (9, 5, 3, 1, 'W'),
        (10, 5, 3, 1, 'B'),
        (11, 5, 3, 1, 'k'),
-       (12, 5, 3, 1, 'Waar');
+       (12, 5, 3, 2, 'Waar');
 
 insert into given_answer (QUESTIONID, PARTICIPANTID, CAMPAIGN_ID, STATEID, GIVEN_ANSWER)
 values (1, 6, 3, 1, 't'),
        (2, 6, 3, 1, 'n'),
-       (3, 6, 3, 1, 'Ja'),
+       (3, 6, 3, 2, 'Ja'),
        (4, 6, 3, 1, 'H'),
        (5, 6, 3, 1, 'd'),
-       (6, 6, 3, 1, 'Eerste'),
+       (6, 6, 3, 3, 'Eerste'),
        (7, 6, 3, 1, 'y'),
        (8, 6, 3, 1, 'x'),
        (9, 6, 3, 1, 'Z'),
        (10, 6, 3, 1, 'f'),
        (11, 6, 3, 1, 'j'),
-       (12, 6, 3, 1, 'Waar');
+       (12, 6, 3, 2, 'Waar');
